2114f0518a719912ac3d67d0e96bd1a12e47c55a
likwidsec <likwidsec@hackermail.com>
2018-01-06 17:16:09 -0500

    Add files via upload
    Added -dp and -ssl versions.  -dp will launch the scans at a "different port", hence, the -dp .. and the -ssl version should be fairly obvious, will launch the scans targeted at ssl services, and adds an ssl-only scan.

--- rats-dp.sh
+++ rats-dp.sh
@@ -7,30 +7,29 @@
 # -- likwidsec --
 #
 # -- 10/27/17 -- Now the script takes 1 argument - the host.  No more editing every time.
+# -- 11/??/17 -- The modified (-dp) script now takes 2 arguments - the host and the port. Used for using tools on diff ports.
 
 HOST=$1
+PORT=$2
 WFUZZWL="/usr/share/wordlists/wfuzz/general/big.txt"
 WORDLIST="/usr/share/wordlists/dirbuster/dir-medium.txt" # -- CHANGE THIS IF NEEDED
 USERLIST="/usr/share/wordlists/metasploit/unix_users.txt"
 PASSLIST="/usr/share/wordlists/rockyou.txt"
 
-#nmap -
-nmap -T3 -A --reason --privileged -vv -oA nmap.$HOST $HOST &
-
 #wpscan -
-wpscan --enumerate --url http://$HOST/ > wpscan.$HOST &
+wpscan --enumerate --threads 16 --url http://$HOST:$PORT/ > wpscan.$HOST-p$PORT &
 
 #wfuzz -
-wfuzz -c -z file,$WFUZZWL --hc 404,500 http://$HOST/FUZZ > wfuzz.$HOST &
+wfuzz -c -z file,$WFUZZWL --hc 404,500 -t 16 http://$HOST:$PORT/FUZZ > wfuzz.$HOST-p$PORT &
 
 #gobuster -
-gobuster -w $WORDLIST -u http://$HOST > gobuster.$HOST &
+gobuster -w $WORDLIST -u http://$HOST:$PORT -t 16 -v > gobuster.$HOST-p$PORT &
 
 #dirb -
-dirb http://$HOST $WORDLIST -w -r > dirb.$HOST &
+dirb http://$HOST:$PORT $WORDLIST -w -r > dirb.$HOST-p$PORT &
 
 #uniscan -
-uniscan -u http://$HOST/ -qweds > uniscan.$HOST &
+uniscan -u http://$HOST:$PORT/ -bqweds > uniscan.$HOST-p$PORT &
 
 #nikto -
-nikto -h $HOST -Cgidirs all -ask auto > nikto.$HOST &
+nikto -h $HOST -p $PORT -Cgidirs all -ask auto -output nikto.$HOST-p$PORT.txt > nikto.$HOST-p$PORT &
--- rats-ssl.sh
+++ rats-ssl.sh
@@ -4,33 +4,35 @@
 # - Add SQLMap and Hydra commands.
 # - Add some sort of menu or choice type system to run or not run certain scans when run.
 # - Make a sandwich.
-# -- likwidsec --
 #
-# -- 10/27/17 -- Now the script takes 1 argument - the host.  No more editing every time.
+# - Version for scanning targets with SSL.
+# - Added a few SSL scans.
+# -- likwidsec --
 
-HOST=$1
+HOST=$1 # Takes first argument as host.
 WFUZZWL="/usr/share/wordlists/wfuzz/general/big.txt"
 WORDLIST="/usr/share/wordlists/dirbuster/dir-medium.txt" # -- CHANGE THIS IF NEEDED
 USERLIST="/usr/share/wordlists/metasploit/unix_users.txt"
 PASSLIST="/usr/share/wordlists/rockyou.txt"
 
-#nmap -
-nmap -T3 -A --reason --privileged -vv -oA nmap.$HOST $HOST &
-
 #wpscan -
-wpscan --enumerate --url http://$HOST/ > wpscan.$HOST &
+wpscan --enumerate --url https://$HOST/ > wpscan-ssl.$HOST &
 
 #wfuzz -
-wfuzz -c -z file,$WFUZZWL --hc 404,500 http://$HOST/FUZZ > wfuzz.$HOST &
+wfuzz -c -z file,$WFUZZWL --hc 404,500 https://$HOST/FUZZ > wfuzz-ssl.$HOST &
 
 #gobuster -
-gobuster -w $WORDLIST -u http://$HOST > gobuster.$HOST &
+gobuster -w $WORDLIST -u https://$HOST > gobuster-ssl.$HOST &
 
 #dirb -
-dirb http://$HOST $WORDLIST -w -r > dirb.$HOST &
+dirb https://$HOST $WORDLIST -w -r > dirb-ssl.$HOST &
 
 #uniscan -
-uniscan -u http://$HOST/ -qweds > uniscan.$HOST &
+uniscan -u https://$HOST/ -qweds > uniscan-ssl.$HOST &
 
 #nikto -
-nikto -h $HOST -Cgidirs all -ask auto > nikto.$HOST &
+nikto -h https://$HOST -Cgidirs all -ask auto > nikto-ssl.$HOST &
+
+#sslscan
+sslscan --show-certificate --verbose $HOST > sslscan.$HOST &
+
